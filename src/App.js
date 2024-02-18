f75e388ea60694535228e8142caa5ef0ac3d8788
Thirupathi Govindharaj <thirupathigovind007@gmail.com>
2024-02-18 22:02:55 +0530

    Added react-bootstrap in UI

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+
+const App = () => {
+  return (
+    <div>
+      <h1>
+        <span> Hello from React! </span>
+      </h1>
+      <Button variant="primary" size="sm">
+        Primary
+      </Button>{' '}
+    </div>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/main.scss';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/main.scss'
+import App from './App'
 
-function App() {
-    return <h1><span> Hello from React! </span></h1>;
-}   
-
-ReactDOM.render(<App/>, document.getElementById('root'));   
+ReactDOM.render(<App />, document.getElementById('root'))
 
 if (module.hot) {
-    module.hot.accept();
+  module.hot.accept()
 }
